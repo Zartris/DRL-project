8c9868aec118e86c854735d85e990123d256a7f2
Zartris <jonas.le.fevre@gmail.com>
2020-01-16 15:12:28 +0100

    new notes

---
+++ DRL-course/Alpha/agents/AgentBase.py
@@ -0,0 +1,98 @@
+import os
+from pathlib import Path
+
+import numpy as np
+import torch
+from skimage.io import imsave
+
+from ..utils import *
+
+
+class AgentBase:
+    def __init__(self,
+                 name: str,
+                 save_path: Path = Path(os.getcwd(), "..", "save_folder")
+                 ):
+        self.name = name
+        self.save_path = save_path
+        self.logger = get_logger(tag=config.tag, log_level=config.log_level)
+        self.task_ind = 0
+        Path("")
+
+    def save(self, filename):
+
+        torch.save(self.network.state_dict(), '%s.model' % (filename))
+        with open('%s.stats' % (filename), 'wb') as f:
+            pickle.dump(self.config.state_normalizer.state_dict(), f)
+
+    def load(self, filename):
+        state_dict = torch.load('%s.model' % filename, map_location=lambda storage, loc: storage)
+        self.network.load_state_dict(state_dict)
+        with open('%s.stats' % (filename), 'rb') as f:
+            self.config.state_normalizer.load_state_dict(pickle.load(f))
+
+    def eval_step(self, state):
+        """
+        This is to be overwritten
+        """
+        raise NotImplementedError
+
+    def eval_episode(self):
+        env = self.config.eval_env
+        state = env.reset()
+        while True:
+            action = self.eval_step(state)
+            state, reward, done, info = env.step(action)
+            ret = info[0]['episodic_return']
+            if ret is not None:
+                break
+        return ret
+
+    def eval_episodes(self):
+        episodic_returns = []
+        for ep in range(self.config.eval_episodes):
+            total_rewards = self.eval_episode()
+            episodic_returns.append(np.sum(total_rewards))
+        self.logger.info('steps %d, episodic_return_test %.2f(%.2f)' % (
+            self.total_steps, np.mean(episodic_returns),
+            np.std(episodic_returns) / np.sqrt(len(episodic_returns))
+        ))
+        self.logger.add_scalar('episodic_return_test', np.mean(episodic_returns), self.total_steps)
+        return {
+            'episodic_return_test': np.mean(episodic_returns),
+        }
+
+    def record_online_return(self, info, offset=0):
+        if isinstance(info, dict):
+            ret = info['episodic_return']
+            if ret is not None:
+                self.logger.add_scalar('episodic_return_train', ret, self.total_steps + offset)
+                self.logger.info('steps %d, episodic_return_train %s' % (self.total_steps + offset, ret))
+        elif isinstance(info, tuple):
+            for i, info_ in enumerate(info):
+                self.record_online_return(info_, i)
+        else:
+            raise NotImplementedError
+
+    def record_episode(self, dir, env):
+        dir = Path(dir)
+        steps = 0
+        state = env.reset()
+        while True:
+            self.record_obs(env, dir, steps)
+            action = self.record_step(state)
+            state, reward, done, info = env.step(action)
+            ret = info[0]['episodic_return']
+            steps += 1
+            if ret is not None:
+                break
+
+    def record_step(self, state):
+        raise NotImplementedError
+
+    # For DMControl
+    @staticmethod
+    def record_obs(env, dir, steps):
+        env = env.env.envs[0]
+        obs = env.render(mode='rgb_array')
+        imsave('%s/%04d.png' % (dir, steps), obs)
